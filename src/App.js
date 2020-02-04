33a10515f3fd8bfb79a32462cc82cac0f6042676
Winston Downes <winston_downes@hotmail.com>
2020-02-05 05:37:57 +1100

    person changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Person from "./Person/Person";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <hi>Hi there, big ears!</hi>
+      <Person />
     </div>
   );
 }
