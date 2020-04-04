5bb02060b47de398a334aa1dbe2abe27eadc101f
Winston Downes <winston_downes@hotmail.com>
2020-04-05 02:08:06 +1000

    style, buttons, handlers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.scss";
 import Course from "./components/Course";
+import { ADDRCONFIG } from "dns";
+
+const initialCourses = [
+  {
+    id: 1,
+    title: "Neural Networks",
+    desc: "This course teaches you to make a neural network"
+  },
+  {
+    id: 2,
+    title: "History of AI",
+    desc: "A look at the last 100 years of AI"
+  }
+];
 
 function App() {
+  const [courses, setCourses] = useState(initialCourses);
+
+  const addCourseHandler = e => {
+    const copyCourses = courses.filter(course => course !== this);
+    setCourses(copyCourses);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="green">Available Courses</h1>
-        <Course
-          title="Neural Networks"
-          desc="This course teaches you to make a neural network"
-        ></Course>
-        <Course
-          title="History of AI"
-          desc="A look at the last 100 years of AI"
-        ></Course>
+        <h1 className="green App-title">Available Courses</h1>
+        {courses.map((course, i) => (
+          <div>
+            <Course
+              title={course.title}
+              desc={course.desc}
+              key={`${i}${course.title}${course.desc}`}
+              onClick={addCourseHandler}
+            ></Course>
+          </div>
+        ))}
       </header>
     </div>
   );
--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Course.scss";
 
 function Course(props) {
+  //   const [flag, setFlag] = useState(false);
+  //   let flag = false;
   return (
-    <div>
+    <div className="course-style">
       <h3>{props.title}</h3>
+      <hr></hr>
       <p>{props.desc}</p>
+      <button onClick={props.onClick}>Add Course</button>
     </div>
   );
 }
